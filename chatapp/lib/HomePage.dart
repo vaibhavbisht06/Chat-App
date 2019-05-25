c42bd5b02d42c93d93b586cdd1ed974c9fe80e46
Vaibhav <vaibhavbisht06@gmail.com>
2019-05-25 21:55:56 +0530

    Basic Structure

---
+++ chatapp/lib/HomePage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Text("Field Connect"),
+        elevation: 10.0,
+      ),
+
+    );
+  }
+}
--- chatapp/lib/main.dart
+++ chatapp/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'HomePage.dart';
 void main(){
   runApp(new Myapp());
 }
